512099b52ab804e45228737a7e1e3948b5d8851b
Matei Stanca <i@ambientimpact.com>
2022-04-25 15:54:35 -0400

    Added rebuilder:list Drush command.

---
+++ src/Commands/ListCommand.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+namespace Drupal\rebuilder\Commands;
+
+use Consolidation\OutputFormatters\StructuredData\UnstructuredListData;
+use Drupal\rebuilder\PluginManager\RebuilderManagerInterface;
+use Drush\Commands\DrushCommands;
+use Drush\Exceptions\CommandFailedException;
+
+/**
+ * rebuilder:list Drush command.
+ *
+ * @see self::listRebuilders()
+ */
+class ListCommand extends DrushCommands {
+
+  /**
+   * The Rebuilder plug-in manager.
+   *
+   * @var \Drupal\rebuilder\PluginManager\RebuilderManagerInterface
+   */
+  protected $rebuilderManager;
+
+  /**
+   * Constructor; saves dependencies.
+   *
+   * @param \Drupal\rebuilder\PluginManager\RebuilderManagerInterface $rebuilderManager
+   *   The Rebuilder plug-in manager.
+   */
+  public function __construct(RebuilderManagerInterface $rebuilderManager) {
+    $this->rebuilderManager = $rebuilderManager;
+  }
+
+  /**
+   * List available Rebuilder types.
+   *
+   * @command rebuilder:list
+   *
+   * @return \Consolidation\OutputFormatters\StructuredData\UnstructuredListData
+   */
+  public function listRebuilders(): UnstructuredListData {
+
+    return new UnstructuredListData(\array_keys(
+      $this->rebuilderManager->getDefinitions()
+    ));
+
+  }
+
+}
