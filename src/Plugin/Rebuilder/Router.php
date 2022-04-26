6b5d474ea5b6c488c7fc4d2d4b9af23bd1d58f6f
Matei Stanca <i@ambientimpact.com>
2022-04-26 12:33:48 -0400

    Added Router Rebuilder plug-in; rebuilds the Drupal router.

---
+++ src/Plugin/Rebuilder/Router.php
@@ -0,0 +1,74 @@
+<?php declare(strict_types=1);
+
+namespace Drupal\rebuilder\Plugin\Rebuilder;
+
+use Drupal\Core\Routing\RouteBuilderInterface;
+use Drupal\Core\StringTranslation\TranslationInterface;
+use Drupal\rebuilder\Plugin\Rebuilder\RebuilderBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Router rebuilder plug-in.
+ *
+ * @Rebuilder(
+ *   id           = "router",
+ *   title        = @Translation("Router"),
+ *   description  = @Translation("Rebuilds the Drupal router.")
+ * )
+ */
+class Router extends RebuilderBase {
+
+  /**
+   * The Drupal route builder service.
+   *
+   * @var \Drupal\Core\Routing\RouteBuilderInterface
+   */
+  protected RouteBuilderInterface $routeBuilder;
+
+  /**
+   * {@inheritdoc}
+   *
+   * @param \Drupal\Core\Routing\RouteBuilderInterface $routeBuilder
+   *   The Drupal route builder service.
+   */
+  public function __construct(
+    array $configuration, string $pluginId, array $pluginDefinition,
+    TranslationInterface  $stringTranslation,
+    RouteBuilderInterface $routeBuilder
+  ) {
+
+    parent::__construct(
+      $configuration, $pluginId, $pluginDefinition,
+      $stringTranslation
+    );
+
+    $this->routeBuilder = $routeBuilder;
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(
+    ContainerInterface $container,
+    array $configuration, $pluginId, $pluginDefinition
+  ) {
+    return new static(
+      $configuration, $pluginId, $pluginDefinition,
+      $container->get('string_translation'),
+      $container->get('router.builder')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function rebuild(array $options = []): void {
+
+    $this->routeBuilder->rebuild();
+
+    $this->setOutput($this->t('Router rebuilt.'));
+
+  }
+
+}
