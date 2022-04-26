575ebc6375a104baf494b4b78d11a527c2d67752
Matei Stanca <i@ambientimpact.com>
2022-04-26 12:53:57 -0400

    Added library definitions Rebuilder plug-in.

---
+++ src/Plugin/Rebuilder/Library.php
@@ -0,0 +1,74 @@
+<?php declare(strict_types=1);
+
+namespace Drupal\rebuilder\Plugin\Rebuilder;
+
+use Drupal\Core\Asset\LibraryDiscoveryInterface;
+use Drupal\Core\StringTranslation\TranslationInterface;
+use Drupal\rebuilder\Plugin\Rebuilder\RebuilderBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Library definition rebuilder plug-in.
+ *
+ * @Rebuilder(
+ *   id           = "library",
+ *   title        = @Translation("Library"),
+ *   description  = @Translation("Rebuilds library definitions.")
+ * )
+ */
+class Library extends RebuilderBase {
+
+  /**
+   * The Drupal library discovery service.
+   *
+   * @var \Drupal\Core\Asset\LibraryDiscoveryInterface
+   */
+  protected LibraryDiscoveryInterface $libraryDiscovery;
+
+  /**
+   * {@inheritdoc}
+   *
+   * @param \Drupal\Core\Asset\LibraryDiscoveryInterface $libraryDiscovery
+   *   The Drupal library discovery service.
+   */
+  public function __construct(
+    array $configuration, string $pluginId, array $pluginDefinition,
+    TranslationInterface      $stringTranslation,
+    LibraryDiscoveryInterface $libraryDiscovery
+  ) {
+
+    parent::__construct(
+      $configuration, $pluginId, $pluginDefinition,
+      $stringTranslation
+    );
+
+    $this->libraryDiscovery = $libraryDiscovery;
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(
+    ContainerInterface $container,
+    array $configuration, $pluginId, $pluginDefinition
+  ) {
+    return new static(
+      $configuration, $pluginId, $pluginDefinition,
+      $container->get('string_translation'),
+      $container->get('library.discovery')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function rebuild(array $options = []): void {
+
+    $this->libraryDiscovery->clearCachedDefinitions();
+
+    $this->setOutput($this->t('Library definitions rebuilt.'));
+
+  }
+
+}
